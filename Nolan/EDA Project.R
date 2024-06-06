d7b24ccc0e5839e86274bee792865dddca54452a
Danny Nolan <dbn005@bucknell.edu>
2024-06-06 15:30:55 -0400

    6/6

--- Nolan/EDA Project.R
+++ Nolan/EDA Project.R
@@ -2,3 +2,8 @@ library(tidyverse)
 nwsl_team_stats <- read_csv("https://raw.githubusercontent.com/36-SURE/36-SURE.github.io/main/data/nwsl_team_stats.csv")
 
 head(nwsl_team_stats) 
+
+levels(factor(nwsl_team_stats$team_name))
+
+attach(nwsl_team_stats)
+levels(factor(team_name))
