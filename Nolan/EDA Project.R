2fa18834613a8b9f2cd1563614a99b0f6e3a58d5
Danny Nolan <dbn005@bucknell.edu>
2024-06-07 15:36:36 -0400

    6/7

--- Nolan/EDA Project.R
+++ Nolan/EDA Project.R
@@ -7,3 +7,20 @@ levels(factor(nwsl_team_stats$team_name))
 
 attach(nwsl_team_stats)
 levels(factor(team_name))
+library(plotly)
+
+gd_vs_pp <- nwsl_team_stats |> 
+  ggplot(aes(x=possession_pct, y=goal_differential))+
+  geom_point(color="red")+
+  labs(
+    title='Does Controlling the Ball Lead to Success?'
+  )+
+  xlab("Possession Percentage")+
+  ylab("Goal Differential")+
+  geom_smooth(method="lm")+
+  theme_gray()+
+    theme(
+    plot.title = element_text(hjust=0.5, face="bold")
+  )
+
+ggplotly(gd_vs_pp)
