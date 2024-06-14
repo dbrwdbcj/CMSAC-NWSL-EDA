3eaeec13362a9e0afae724a38c1d91567710ce84
Neha Kotha <nehakotha7@gmail.com>
2024-06-14 15:13:43 -0400

    edits to categorical data

--- Kotha/EDA_project.R
+++ Kotha/EDA_project.R
@@ -12,6 +12,11 @@ median_goal_differential <-nwsl_team_stats |>
   summarize(median_goal_differential = median(goal_differential, na.rm = TRUE))
   print(median_goal_differential) 
   
+  median_goal_conversion_pct <-nwsl_team_stats |> 
+    group_by(season) |> 
+    summarize(median_goal_conversion_pct = median(goal_conversion_pct, na.rm = TRUE))
+  print(median_goal_conversion_pct ) 
+  
   # ggplot(median_goal_differential, aes(x = season, y = median_goal_differential)) +
   #   geom_line() +
   #   geom_point() +
@@ -49,6 +54,56 @@ median_goal_differential <-nwsl_team_stats |>
   #   ggtitle('Layered Line Graphs: Goal Differentials') +
   #   theme(legend.position = "right") 
   
+  median_goals <-nwsl_team_stats |> 
+    group_by(season) |> 
+    summarize(median_goals = median(goals, na.rm = TRUE))
+  print(median_goals) 
+  
+  pass_pct_half_diff <- nwsl_team_stats |> 
+    group_by(team_name, season) |> 
+    summarize(pass_pct_half_diff = mean(pass_pct - pass_pct_opposition_half, na.rm = TRUE)) |>
+    arrange(team_name, season)
+  print(pass_pct_half_diff)
+  
+ nwsl_team_stats <- nwsl_team_stats |> 
+      mutate(pass_pct_half_diff = pass_pct - pass_pct_opposition_half)
+  ggplot()+
+  #(nwsl_team_stats |> 
+       # filter(team_name %in% c("Chicago Red Stars", "Houston Dash", "NJ/NY Gotham FC", "OL Reign", "Orlando Pride", "Portland Thorns FC", "Washington Spirit")),
+     #    aes(x = season, y = pass_pct_half_diff, fill = team_name)) +
+  #  geom_bar(stat = "identity", position = "dodge") +
+  geom_line(data = nwsl_team_stats |> 
+                filter(team_name %in% c("Chicago Red Stars", "Houston Dash", "NJ/NY Gotham FC", "OL Reign", "Orlando Pride", "Portland Thorns FC", "Washington Spirit")), 
+              aes(x = season, y = pass_pct_half_diff, group = team_name, color = team_name), linewidth = 1) +
+    labs(
+      title = "Difference in Pass Accuracy Between First and Second Half by Season",
+      x = "Season",
+      y = "Pass Accuracy Difference (%)",
+      fill = "Team Name"
+    ) +
+    theme_bw() 
+  
+ggplot(nwsl_team_stats |> 
+       filter(!team_name %in% c("Chicago Red Stars", "Houston Dash", "NJ/NY Gotham FC", "OL Reign", "Orlando Pride", "Portland Thorns FC", "Washington Spirit")), 
+       aes(x = season, y = pass_pct_half_diff, fill = team_name)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Goals Scored by Each Team per Season",
+       x = "Season",
+       y = "Goals Scored",
+       fill = "Team Name") +
+  theme_bw()
+
+
+ggplot(nwsl_team_stats |> 
+         filter(team_name %in% c("Chicago Red Stars", "Houston Dash", "NJ/NY Gotham FC", "OL Reign", "Orlando Pride", "Portland Thorns FC", "Washington Spirit")), 
+       aes(x = season, y = pass_pct_half_diff, fill = team_name)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Goals Scored by Each Team per Season",
+       x = "Season",
+       y = "Goals Scored",
+       fill = "Team Name") +
+  theme_bw()
+
   # Season Goal Differential of Folded Teams with median legend
   ggplot() +
     # First dataset: median_goal_differential
@@ -78,7 +133,6 @@ median_goal_differential <-nwsl_team_stats |>
     ggtitle('Folded Teams Goal Differential Compared to League Median') +
     theme(legend.position = "right", 
           plot.title = element_text(hjust = 0, face = 'bold')) 
-      
     
   
   # Season Goal Differential of Lasting Teams with median legend
